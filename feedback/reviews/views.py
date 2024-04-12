029f4a675a44330b600552a4268a0a94a2cee8e6
adity418 <adity119@gmail.com>
2024-04-12 23:04:32 +0530

    session added to feedback review app

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -104,6 +104,15 @@ class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite-review")
+        context["is_favorite"] = favorite_id == str(loaded_review)
+        return context
+
+
 
     # def get_context_data(self, **kwargs):   # TemplateView
     #     context = super().get_context_data(**kwargs)
@@ -111,3 +120,9 @@ class SingleReviewView(DetailView):
     #     selected_review = Review.objects.get(pk=review_id)
     #     context["reviews"] = selected_review
     #     return context
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect ("/reviews/" + review_id)
