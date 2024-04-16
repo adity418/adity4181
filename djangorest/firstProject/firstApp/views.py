e4cd2d584bfa91f1724eda5b1c711b393c93d55a
adity418 <adity119@gmail.com>
2024-04-16 20:10:40 +0530

    create an project using django and mysql

---
+++ djangorest/firstProject/firstApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from firstApp.models import Employee
+
+# Create your views here.
+def employeeView(request):
+    emp = {
+        'id':123,
+        'name':'Aditya',
+        'sal': 1000000,
+    }
+
+    data = Employee.objects.all();
+    response = {'employees':list(data.values('name', 'sal'))}
+
+
+    return JsonResponse(response)
